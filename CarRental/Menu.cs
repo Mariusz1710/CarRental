010714f3e8d6dce8bb1f81f56ad08c3708380ac3
Mariusz1710 <mbielicki@wp.pl>
2024-02-25 15:14:41 +0100

    Initial commit

---
+++ CarRental/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental
+{
+    internal class Menu
+    {
+        public static void ShowMenu()
+        {
+            Console.WriteLine("WYBIERZ OPCJĘ:");
+            Console.WriteLine("1 => LISTA KLIENTÓW I SAMOCHODÓW");
+            Console.WriteLine("2 => WYPOŻYCZENIE SAMOCHODU");
+            Console.WriteLine("3 => ZAKOŃCZ PROGRAM");
+            Console.WriteLine("WYBIERZ 1, 2 LUB 3");
+            
+        }
+    }
+}
